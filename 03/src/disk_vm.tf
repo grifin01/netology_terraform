883df5c35615cb12c28954a27474685f80b02738
grifin01 <111656372+grifin01@users.noreply.github.com>
2024-02-28 02:28:16 +0600

    Update disk_vm.tf

--- 03/src/disk_vm.tf
+++ 03/src/disk_vm.tf
@@ -1,16 +1,16 @@
 resource "yandex_compute_disk" "storage_1" {
   count   = 3
-  name  = "disk-${count.index + 1}"
-  size  = 1
+  name    = "disk-${count.index + 1}"
+  size    = var.vms_resources.storage_vm.disk_volume
 }
 
 
 resource "yandex_compute_instance" "storage" {
   name = "storage"
   resources {
-    cores = 2
-    memory = 1
-    core_fraction = 5
+    cores         = var.vms_resources.storage_vm.cpu
+    memory        = var.vms_resources.storage_vm.ram
+    core_fraction = var.vms_resources.storage_vm.core_fraction
   }
 
   boot_disk {
@@ -20,9 +20,10 @@ resource "yandex_compute_instance" "storage" {
   }
 
   dynamic "secondary_disk" {
-   for_each = { for stor in yandex_compute_disk.storage_1[*]: stor.name=> stor }
+    for_each = toset(yandex_compute_disk.storage_1[*].id)
+#   for_each = { for stor in yandex_compute_disk.storage_1[*]: stor.name=> stor }
    content {
-     disk_id = secondary_disk.value.id
+     disk_id = secondary_disk.key
    }
   }
   network_interface {
